3c2120ddc415845f93267f0aeda54a380c4a39ba
Anne Crescent <crescenthadex@gmail.com>
2022-07-09 11:28:05 +0530

    new

---
+++ src/main/java/com/interview/booking/MovieBookingConfigApplication.java
@@ -0,0 +1,13 @@
+package com.interview.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBookingConfigApplication.class, args);
+	}
+
+}
